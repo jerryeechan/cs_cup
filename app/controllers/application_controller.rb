f2eebba8de59e4de51ae34f6be5f5ae8a248b5ae
jerrypupu111 <pupu1416@yahoo.com.tw>
2015-09-22 17:08:08 +0800

    fix image, add jack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
  # protect_from_forgery with: :exception
-	@@uidList = ["1073719775978420","1177007412316322"]
+	@@uidList = ["1073719775978420","1177007412316322","731459686959084","932544443484230"]
+          # => me dev,             me production,     會長 production,    jack produciton
   private
   def current_user
     @current_user ||= User.find_by(uid: session[:user_id]) if session[:user_id]
