c79e99f7dbc7660944dbda587f6250b027479a69
jerrypupu111 <pupu1416@yahoo.com.tw>
2015-10-29 00:00:23 +0800

    add admin

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,21 +80,6 @@ class User < ActiveRecord::Base
     
     if exists?(uid: auth.uid)
 #!!!!! new registration closed
-=begin
-
-    else
-      User.new.tap do |user|
-        user.uid = auth.uid
-        user.school = "請點擊填入學校"
-      user.department = "請點擊填入系所"
-      user.sport = "請點擊選擇球類項目"
-      user.transfercode = "請點擊填入轉帳代碼"
-      user.captain = "請點擊填入隊長姓名"
-      user.save!
-      end
-    end
-=end
-
       where(uid: auth.uid).first.tap do |user|
 
         #user.email = auth.info.email
@@ -119,8 +104,21 @@ class User < ActiveRecord::Base
 
         
         user.save!
-    	end
+      end
+    else
+      User.new.tap do |user|
+        user.uid = auth.uid
+        user.school = "請點擊填入學校"
+      user.department = "請點擊填入系所"
+      user.sport = "請點擊選擇球類項目"
+      user.transfercode = "請點擊填入轉帳代碼"
+      user.captain = "請點擊填入隊長姓名"
+      user.save!
+      end
     end
+
+
+      
   end
 
   has_many :members
