e795d5c1ef33be7f09fb40d706e8f9edaabd5786
jerrypupu111 <pupu1416@yahoo.com.tw>
2015-10-29 23:12:46 +0800

    send all

---
+++ app/mailers/notify_all.rb
@@ -0,0 +1,6 @@
+class NotifyAll < ApplicationMailer
+	def confirm(email)
+    
+    mail(:to =>email,:subject=>"中資盃  秩序手冊")
+  end
+end
---
+++ test/mailers/notify_all_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class NotifyAllTest < ActionMailer::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
